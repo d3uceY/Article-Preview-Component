595f1d1bd83d8e8ca759dc0770de16ead2c21e3a
d3uceY <onyekwelujesse1234@gmail.com>
2023-07-10 11:30:19 -0700

    i made some changes to this repository

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const shareButton = document.querySelector('.share-icon');
+const shareButton = document.querySelector('.share-button-1');
 const socialContainer = document.querySelector('.socials-container');
+const secondShareButton = document.querySelector('.share-button-2');
 
 shareButton.addEventListener('click', () => {
     socialContainer.classList.toggle('clicked');
+})
+
+
+
+secondShareButton.addEventListener('click', () => {
+    socialContainer.classList.remove('clicked');
 })
